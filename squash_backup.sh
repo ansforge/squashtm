95da6823862d7ca66d6466a719964e5311e37cbc
Yann Etrillard <yetrillard@henix.fr>
2023-09-25 14:58:11 +0200

    Modification du chemin de sauvegarde dans le script de backup

--- squash_backup.sh
+++ squash_backup.sh
@@ -4,7 +4,7 @@ echo "Démarrage du script de sauvegarde de Squash"
 # Nom du script     : squash-backup.sh
 # Auteur            : E.RIEGEL (QM HENIX)
 # Date de Création  : 24/02/2023
-# Version           : 0.0.1
+# Version           : 0.0.2
 # Descritpion       : Script permettant la sauvegarde de la BDD de Squash
 #
 # Historique des mises à jour :
@@ -12,6 +12,8 @@ echo "Démarrage du script de sauvegarde de Squash"
 #  Version  |   Date   |   Auteur     |  Description
 #-----------+--------+-------------+------------------------------------------------------
 #  0.0.1    | 24/02/23 | E.RIEGEL     | Initialisation du script
+#-----------+--------+-------------+------------------------------------------------------
+#  0.0.2    | 21/09/23 | Y.ETRILLARD  | Ajout -task dans la commande nomad exec
 #-----------+--------+-------------+------------------------------------------------------
 #
 ###############################################################################################
@@ -41,7 +43,7 @@ mkdir -p $BACKUP_DIR/$DATE
 
 # Dump Squash bdd
 echo "starting Squash dump..."
-$NOMAD exec -job forge-squashtm-postgresql  pg_dump -F c --dbname=postgresql://postgres@localhost/squashtm > $BACKUP_DIR/$DATE/$DUMP_FILENAME
+$NOMAD exec -task postgres -job forge-squashtm-postgresql  pg_dump -F c --dbname=postgresql://postgres@localhost/squashtm > $BACKUP_DIR/$DATE/$DUMP_FILENAME
 
 DUMP_RESULT=$?
 if [ $DUMP_RESULT -gt 0 ]
